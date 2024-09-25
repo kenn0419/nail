2608808d784f0544a4c67d528671b45bd2528727
Quoc Bao <tranbao7008@gmail.com>
2024-09-25 17:23:52 +0700

    fix last one

--- src/public/Thanks.jsx
+++ src/public/Thanks.jsx
@@ -8,7 +8,7 @@ const Thanks = () => {
             <div className='absolute inset-0 z-1 bg-[#fff] opacity-70'></div>
             <div className='bg-[#fff] relative z-[5] mt-[100px] flex flex-col justify-center items-center gap-12 w-main mx-auto p-5 shadow-md tablet:w-[100%] phone:w-[100%]'>
                 <h4 className='text-[36px] text-center'>Thank you for booking a service at Hollywood nails & Beauty, we have scheduled an appointment for you. Please pay attention and arrive on time. Thank you very much.</h4>
-                <Link to={`/${path.HOME}`} className='px-6 py-3 bg-main text-[25px]'>Back To Homepage</Link>
+                <Link to={`/${path.HOME}`} className='px-6 py-3 bg-[#000] text-[#fff] text-[25px]'>Back To Homepage</Link>
             </div>
             <div className='mb-[200px]'></div>
         </div>
